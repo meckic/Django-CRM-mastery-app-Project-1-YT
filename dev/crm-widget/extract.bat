9897ed961ce625bb217bf0fdfe617bba955eccf6
mecki <mecki@gmail.com>
2024-09-29 07:44:29 +0300

    all deleted (admin widget)

--- dev/crm-widget/extract.bat
+++
@@ -1,5 +0,0 @@
-c:\Users\mecki\sqlite\sqlite3.exe db.sqlite3 "SELECT * FROM auth_user;" > auth_user.csv
-c:\Users\mecki\sqlite\sqlite3.exe db.sqlite3 "SELECT * FROM webapp_person;" > person.csv
-c:\Users\mecki\sqlite\sqlite3.exe db.sqlite3 "SELECT * FROM webapp_event;" > event.csv
-c:\Users\mecki\sqlite\sqlite3.exe db.sqlite3 "SELECT * FROM webapp_event_attendees;" > event_attendees.csv
-c:\Users\mecki\sqlite\sqlite3.exe db.sqlite3 "SELECT * FROM webapp_venue;" > venue.csv
